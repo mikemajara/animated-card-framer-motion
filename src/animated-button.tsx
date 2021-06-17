19599c5be96c266960ce047daf8e1d6086a8ec2d
mikemajara <fmiguel05@gmail.com>
2021-06-17 12:26:47 +0200

    working card without layout
    * remove padding glitch

--- src/animated-button.tsx
+++ src/animated-button.tsx
@@ -47,7 +47,7 @@ export const AnimatedButton = (props: any) => {
     setIsExpanded(props.expanded);
   }, [props.expanded]);
 
-  // setInterval(() => toggleExpanded(), 2000)
+  // setInterval(() => toggleExpanded(), 4000)
 
   const textControls = useAnimation();
 
@@ -58,6 +58,7 @@ export const AnimatedButton = (props: any) => {
 
   const buttonStyle = {
     padding: "8px 16px",
+    // paddingBottom: 0,
     display: "flex",
     flexDirection: "column",
     alignItems: "flex-start",
@@ -81,27 +82,32 @@ export const AnimatedButton = (props: any) => {
         // paddingRight: isExpanded ? "16px" : "11px",
         // paddingLeft: isExpanded ? "16px" : "11px",
         // borderRadius: "10px",
+        // height not animated here because the meassurement
+        // leads to a glitch adjusting to the padding (which is not taken
+        // into account with padding)
+        height: "auto", 
         backgroundColor: "gray.500",
         overflow: "hidden",
       }}
       initial={false}
-      animate={{height: isExpanded ? "auto" : "40px", borderRadius: "10px"}}
-      transition={{duration: ".2"}}
+      // animate={{height: isExpanded ? "auto" : "40px", borderRadius: "10px"}}
+      transition={{duration: "3"}}
     >
       <AFlex align="center" layout initial={false}>
         <AIcon layout as={SunIcon} mr="16px"
           animate={{height: isExpanded ? "1.2rem" : "1rem"}}
         />
-        <AText layout initial={false} animate={{fontSize: isExpanded ? "1.5rem" : "1rem"}} transition={{bounce: 0}}>
+        <AText layout initial={false} animate={{fontSize: isExpanded ? "1.5rem" : "1rem"}}>
           {props.title}
         </AText>
       </AFlex>
       <AText
-        layout
-        py="5"
-        initial={false}
+        // layout
+        // py="5"
+        // initial={false}
         animate={{
           opacity: isExpanded ? 1 : 0,
+          height: isExpanded ? "auto" : "1px"
         }}
         >
         {props.content}
